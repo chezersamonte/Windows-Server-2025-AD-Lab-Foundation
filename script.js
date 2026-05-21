8cd46346d7a272323551baf84dbfaf62b66d43cc
Chezer O. Samonte <62951805+chezersamonte@users.noreply.github.com>
2026-05-21 19:27:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+console.log("Windows Server Home Lab Loaded");
 
+/* Active navigation highlight */
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".sidebar nav a");
+
+window.addEventListener("scroll", () => {
+
+  let current = "";
+
+  sections.forEach(section => {
+
+    const sectionTop = section.offsetTop;
+
+    if (pageYOffset >= sectionTop - 200) {
+      current = section.getAttribute("id");
+    }
+
+  });
+
+  navLinks.forEach(link => {
+
+    link.classList.remove("active");
+
+    if (link.getAttribute("href") === `#${current}`) {
+      link.classList.add("active");
+    }
+
+  });
+
+});
