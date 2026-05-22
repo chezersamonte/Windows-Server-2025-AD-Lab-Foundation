2f619fe7e0976b97d08124478f14a58a33e673e6
Chezer O. Samonte <62951805+chezersamonte@users.noreply.github.com>
2026-05-22 19:29:30 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 console.log("Windows Server Home Lab Loaded");
 
-/* Active navigation highlight */
+/* =========================
+   ACTIVE NAVIGATION
+========================= */
 
 const sections = document.querySelectorAll("section");
+
 const navLinks = document.querySelectorAll(".sidebar nav a");
 
 window.addEventListener("scroll", () => {
@@ -28,5 +31,51 @@ window.addEventListener("scroll", () => {
     }
 
   });
+
+});
+
+/* =========================
+   IMAGE MODAL
+========================= */
+
+const modal = document.getElementById("imageModal");
+
+const modalImg = document.getElementById("modalImage");
+
+const images = document.querySelectorAll(
+  ".post-card img, .vm-card img, .topology-image"
+);
+
+const closeModal = document.querySelector(".close-modal");
+
+/* OPEN MODAL */
+
+images.forEach(image => {
+
+  image.addEventListener("click", () => {
+
+    modal.style.display = "flex";
+
+    modalImg.src = image.src;
+
+  });
+
+});
+
+/* CLOSE BUTTON */
+
+closeModal.addEventListener("click", () => {
+
+  modal.style.display = "none";
+
+});
+
+/* CLOSE WHEN CLICK OUTSIDE */
+
+modal.addEventListener("click", (e) => {
+
+  if (e.target !== modalImg) {
+    modal.style.display = "none";
+  }
 
 });
